27d1eff5bb89d99349921d8abeb74af4743d4448
unknown <culoxo@hotmail.com>
2024-01-25 01:06:50 +0100

    Para presentar

--- src/main/java/com/viacoreit/scs/admin/gw/dto/UserDTO.java
+++ src/main/java/com/viacoreit/scs/admin/gw/dto/UserDTO.java
@@ -26,7 +26,7 @@ public class UserDTO {
 	@NotBlank
 	private String password;
 	
-	private Boolean admin;
+	private Boolean admin = false;
 	private Boolean deleted = false;
 	private Boolean active;
 	private String preguntaSeg;
